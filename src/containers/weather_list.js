2dae55edd010e6eb7c110fb20f3eee09e64dd06e
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-08 19:06:53 -0300

    chore(WeatherList): convert kelvin to celsius

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,14 +5,14 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), temp => temp - 273.15);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
         
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td><Chart data={temps} color="green" units="K" /></td>
+        <td><Chart data={temps} color="green" units="ºC" /></td>
         <td><Chart data={pressures} color="grey" units="hPa" /></td>
         <td><Chart data={humidities} color="blue" units="%" /></td>
       </tr>
@@ -25,7 +25,7 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature (K)</th>
+            <th>Temperature (C)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
           </tr>
